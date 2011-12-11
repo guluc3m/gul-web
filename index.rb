94b2e41d469166e396ed9ea1aabfecff69990eac
David Muñoz Díaz <david@gul.es>
2011-12-11 13:23:49 +0100

    photostream

--- index.rb
+++ index.rb
@@ -9,17 +9,13 @@ include Twitter
 @sections = read_sections
 @timeline = get_timeline
 @announce = read_conditionally "announce.txt"
+@story = @timeline.last
+@photos = @timeline.collect do |story|
+  story.pictures
+end
+@photos.flatten!
 
 template = ERB.new(File.new("index.erb").read)
 puts template.result(binding)
-
-
-
-
-
-
-
-
-
 
 
--- story.rb
+++ story.rb
@@ -8,9 +8,18 @@ file = ARGV[0]
 
 @story = get_story(file) || exit
 @sections = read_sections
+
+# stories in the same section
 @timeline = get_timeline.select do |story|
-  story.section == @story.section
+  story.section == @story.section && story.identifier != @story.identifier
 end
+
+# and their pictures
+@photos = @timeline.collect do |story|
+  story.pictures
+end
+@photos.flatten!
+
 @announce = read_conditionally "announce.txt"
 
 template = ERB.new(File.new("story.erb").read)
