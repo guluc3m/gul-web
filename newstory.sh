d22fc8ec073fefc4e419c7d74cd7bfa1dcf928e4
David Munoz <dmunoz@pc-ubuntu.(none)>
2012-09-28 10:42:42 +0200

    Added hm2/1

---
+++ newstory.sh
@@ -0,0 +1,10 @@
+cat=${1:-hm}
+id=${2:-$(date | md5sum)}
+id=`echo $id | awk '{print $1}'`
+
+echo $cat/$id >> stories/timeline
+mkdir -p stories/$cat
+echo "title = \"\"
+
+body" > stories/$cat/$id
+
